9828bc526d47c171ccff0245abcdc9d766565274
hasan-furkan <hsnfrkn32@gmail.com>
2023-02-07 05:26:37 +0300

    client folder initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Auth } from "./layouts/Auth";
-
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./assets/styles/tailwind.css";
-import "react-toastify/dist/ReactToastify.css";
-import { Register } from "./views/auth/Register";
-import { Login } from "./views/auth/Login";
-import { Dashboard } from "./views/dashboard/Dashboard";
-import { Form } from "./views/formManagement/Form";
+/*!
+=========================================================
+* Muse Ant Design Dashboard - v1.0.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import Billing from "./pages/Billing";
+import Rtl from "./pages/Rtl";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Auth />}>
-          <Route path="/auth/register" element={<Register />} />
-          <Route path="/auth/login" element={<Login />} />
-        </Route>
-        <Route path={"/dashboard"} element={<Dashboard />} />
-        <Route path={"/form"} element={<Form />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Switch>
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/billing" component={Billing} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main>
+      </Switch>
+    </div>
   );
 }
 
